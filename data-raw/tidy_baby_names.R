accc89e3684883fae391f084428304ea5a9768f3
Christy Lam <cyyllam@gmail.com>
2021-02-03 00:51:54 -0800

    revised comments

--- data-raw/tidy_baby_names.R
+++ data-raw/tidy_baby_names.R
@@ -1,4 +1,4 @@
-# this script will compile each file of baby names by state from the Social Security Administration into a 
+# This script will compile each file of baby names by state from the Social Security Administration into a 
 # single table
 # https://catalog.data.gov/dataset/baby-names-from-social-security-card-applications-data-by-state-and-district-of-
 # https://www.ssa.gov/oact/babynames/limits.html
@@ -8,13 +8,15 @@ library(here)
 
 data.dir <- here('data-raw', 'names-by-state') 
 
-# list all files in the directory with their absolute path
+# List all files in the directory with their absolute path
 files <- list.files(data.dir, pattern = '.TXT', full.names = TRUE)
 
-# purrr::partial() is an adverb. We create a new verb that fixes certain arguments of the original verb.
-# This is convenient when we use the purrr::map() in the next step where we use a function to call another function
-# to do stuff.
-read_state_files <- partial(read_delim, 
+# purrr::partial() is an adverb. We create a new verb that modifies the default arguments of the original verb.
+# This is convenient for when we use purrr::map() in the next step. 
+# map() calls another function to do all the stuff. 
+
+# New function: read_state_file()
+read_state_file <- partial(read_delim, 
                             delim = ',', 
                             col_types = cols(
                               X1 = col_character(),
@@ -25,16 +27,17 @@ read_state_files <- partial(read_delim,
                             ),
                             col_names = FALSE)
 
-# instead of writing a for loop to read each state file, purrr::map() can do that. Each tibble is stored in a 
-# list. Then the verb inside purrr::reduce() will be applied to each tibble recursively so that we end up with one
-# main tibble.
-babynames <- map(files, read_state_files) %>% 
+# Instead of writing read_delim() 50 times or writing a for loop to read each state file, use purrr::map(). 
+# The end result of map() is that each tibble is stored in a list. 
+# The verb inside purrr::reduce() will be applied to each tibble recursively. 
+# In the end we have one main tibble.
+babynames <- map(files, read_state_file) %>% 
   reduce(bind_rows) %>% 
   mutate(X2 = if_else(X2 == 'F', 'Female', 'Male'))
 
-# let's add column names! It's just easier using base R.
+# Let's add column names! It's just easier using base R.
 colnames(babynames) <- c('state', 'sex', 'year', 'name', 'count')
 
-# write out data
+# Write out data
 write_csv(babynames, here('data', 'babynames.csv'))
 
