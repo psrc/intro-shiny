09ac5f206e3769e468ea168ffdab3a38c70168b3
Christy Lam <cyyllam@gmail.com>
2021-02-08 23:19:24 -0800

    initialized scripts

---
+++ babynames/app-baby-00.R
@@ -0,0 +1,29 @@
+library(shiny)
+library(tidyverse)
+library(here)
+
+# df <- read_csv(here('data', 'babynames.csv'))
+
+
+# Define UI for application -----------------------------------------------
+
+
+ui <- fluidPage(
+
+  
+)
+
+
+# Define server logic -----------------------------------------------------
+
+
+server <- function(input, output) {
+  
+  
+}
+
+
+#  Run the application  ---------------------------------------------------
+
+
+shinyApp(ui = ui, server = server)
---
+++ babynames/app-baby-01.R
@@ -0,0 +1,38 @@
+library(shiny)
+library(tidyverse)
+library(here)
+
+df <- read_csv(here('data', 'babynames.csv'))
+
+
+# Define UI for application -----------------------------------------------
+
+
+ui <- fluidPage(
+  
+    titlePanel("U.S. Baby Names"),
+    p("Source:", 
+      tags$a(href = "https://www.ssa.gov/oact/babynames/limits.html", 
+             "Social Security Administration")),
+    
+    tableOutput("main_table")
+  
+)
+
+
+# Define server logic -----------------------------------------------------
+
+
+server <- function(input, output) {
+  
+  output$main_table <- renderTable({
+    df %>% slice(1:20)
+    })
+  
+}
+
+
+#  Run the application  ---------------------------------------------------
+
+
+shinyApp(ui = ui, server = server)
